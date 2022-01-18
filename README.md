328b6cbfea342ca1e577e08c285903a6ce88c9fa
pan-dya <pandya.limawan@gmail.com>
2022-01-18 21:25:11 +0700

    Update every code

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Class = L1CC Course = Human Computer Interaction Team = Classifiers
 
 Member : Alfin = 2502036842 Pandya = 2502022433
 
-Description:
+Description: An app that allow the user to track their items, very simple to use. 
+                This is a prototype of the website.
 
-Features:
+Features: The feature available on the demo are login, navigation on the webpage, and input contact form
--- login.js
+++ login.js
@@ -1,3 +1,4 @@
+// put all the id needed into a constant
 const login_box = document.getElementById("login_box");
 const login_box_submit = document.getElementById("login_box_submit");
 const login_error_box = document.getElementById("login_error_box");
@@ -7,9 +8,11 @@ login_box_submit.addEventListener("click", (e) => {
     const username = login_box.username.value;
     const password = login_box.password.value;
 
+    // first condition, to see if the username and password input are correct
     if (username === "testing" && password === "12345678") {
         alert("You have successfully logged in.");
         location.reload();
+    // anything otherwise will return as false, the error box will be shown
     } else {
         login_error_box.style.opacity = 1;
     }
